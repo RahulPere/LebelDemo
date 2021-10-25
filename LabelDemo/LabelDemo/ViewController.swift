913a127bc91ee25205b08cef037fd07492ef1201
Rahul Rathod <rahul.sph@perennialsys.com>
2021-10-25 22:45:34 +0530

    changes

--- LabelDemo/LabelDemo/ViewController.swift
+++ LabelDemo/LabelDemo/ViewController.swift
@@ -13,11 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("No change")
-        print("Another")
     }
-
-
 }
 
 class newApp {
